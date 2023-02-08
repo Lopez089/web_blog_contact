8dbc7a76771ed7c13b820b5da5efe37f28573d48
LopezDev <aragonlopezjuan87@gmail.com>
2023-02-08 12:15:50 +0100

    fix config tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({
-    integrations: [tailwind()],
-    site: 'https://github.com/Lopez089',
-    base: '/web_blog_contact',
 
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  site: 'https://github.com/Lopez089',
+  base: '/web_blog_contact'
 });
